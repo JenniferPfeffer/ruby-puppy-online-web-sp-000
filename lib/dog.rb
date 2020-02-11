fbf556993aae95e035841c71028e04822d5ef63a
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-11 16:03:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Add your code here
+# Add your code here 
+class Dog 
+  @@all = [ ]
+  attr_reader :name
+  def initialize(name) 
+    @name = name
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
+end
+  
